f6864448e2a921f9b70119021bf6db3b0d07cf23
Willian Siqueira <wsiqueira@sogringagem.com.br>
2025-03-22 21:51:05 -0300

    [Context] Store favorites
    Function check hydrate

--- src/app/favorites/page.tsx
+++ src/app/favorites/page.tsx
@@ -1,15 +1,37 @@
 'use client';
 
-import useFavoritesStore from '@/store/favorites';
+import Link from 'next/link';
 
-import { CardList } from '@/components';
+import useFavoritesStore from '@/store/favorites';
+import { CardList, Loading } from '@/components';
 
 export default function PageFavorites() {
   const favorites = useFavoritesStore((state) => state.favorites);
+  const hasHydrated = useFavoritesStore((state) => state._hasHydrated);
+
+  if (!hasHydrated) {
+    return (
+      <main className="container grid place-content-center gap-8 mx-auto md:max-w-6xl">
+        <Loading type="default" />
+      </main>
+    );
+  }
 
   return (
-    <main className="container mx-auto md:max-w-6xl">
-      <CardList data={favorites} />
+    <main className="container grid place-content-center gap-8 mx-auto md:max-w-6xl">
+      {favorites.length === 0 ? (
+        <div className="text-center">
+          <h1 className="heading1">You haven't marked your favorites yet!</h1>
+          <p>
+            Take a look at the <Link href="/" className="link">Characters</Link> page
+          </p>
+        </div>
+      ) : (
+        <>
+          <CardList data={favorites} />
+          <div>[Pagination]</div>
+        </>
+      )}
     </main>
   );
 }
--- src/store/favorites.ts
+++ src/store/favorites.ts
@@ -1,24 +1,41 @@
 import { create } from 'zustand';
-import { persist, createJSONStorage } from 'zustand/middleware'
+import { persist, createJSONStorage } from 'zustand/middleware';
+
+import type { CardItemType } from '@/components/CardList/types';
 
 type favoritesStoreType = {
-  favorites: any;
-  favoriteAdd: (favorite: any) => void;
-  favoriteRemove: (favoriteId: any) => void;
+  favorites: CardItemType[];
+  favoriteAdd: (favorite: CardItemType) => void;
+  favoriteRemove: (favoriteId: string) => void;
+
+  _hasHydrated: boolean
+  setHasHydrated: (state: boolean) => void;
 };
 
 const useFavoritesStore = create<favoritesStoreType>()(
   persist(
     (set, get) => ({
       favorites: [],
-      favoriteAdd: (favorite) => set((state) => ({ favorites: [...state.favorites, favorite] })),  
-      favoriteRemove: (favoriteURL) => set((state) => ({ favorites: state.favorites.filter((favorite) => favorite.url !== favoriteURL) })),
+      favoriteAdd: (favorite) =>
+        set((state) => ({ favorites: [...state.favorites, favorite]})),
+      favoriteRemove: (favoriteURL) =>
+        set((state) => ({ favorites: state.favorites.filter((favorite) => favorite.url !== favoriteURL)})),
+
+      _hasHydrated: false,
+      setHasHydrated: (state) => {
+        set({
+          _hasHydrated: state,
+        });
+      },
     }),
     {
       name: 'favorites',
       storage: createJSONStorage(() => localStorage),
-    },
-  ),
-)
+      onRehydrateStorage: (state) => {
+        return () => state.setHasHydrated(true)
+      }
+    }
+  )
+);
 
 export default useFavoritesStore;
