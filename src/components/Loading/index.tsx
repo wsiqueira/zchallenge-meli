8d3fa3d589e27a51317b00ce6a59467c9e75e323
Willian Siqueira <wsiqueira@sogringagem.com.br>
2025-03-22 21:48:32 -0300

    [Comp] Loading

---
+++ src/components/Loading/index.tsx
@@ -0,0 +1,25 @@
+import Image from 'next/image';
+import { LoaderCircle } from 'lucide-react';
+
+type LoadingType = React.ReactNode & {
+  variant?: 'default' | 'lightsaber' | 'spaceship';
+};
+
+export function Loading({variant = 'default'}): LoadingType { 
+  switch (variant) {
+    case 'lightsaber':
+      return <div>[lightsaber...]</div>;
+    case 'spaceship':
+      return (
+        <Image
+          src="/img_x-wing.gif"
+          alt="X-wing"
+          width={800}
+          height={600}
+          priority
+        />
+      );
+    default:
+      return <LoaderCircle className='animate-spin' />;
+  }
+}
--- src/components/index.ts
+++ src/components/index.ts
@@ -1,4 +1,5 @@
 export { Header } from './Header';
 export { Footer } from './Footer';
 
+export { Loading } from './Loading';
 export { CardList } from './CardList';
