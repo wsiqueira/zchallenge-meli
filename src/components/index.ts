8bb81d76b352adbddb2b707fc1793a3f83ac9e22
Willian Siqueira <wsiqueira@sogringagem.com.br>
2025-03-23 17:45:11 -0300

    [Breadcrumb] Component structure

--- src/components/index.ts
+++ src/components/index.ts
@@ -2,4 +2,5 @@ export { Header } from './Header';
 export { Footer } from './Footer';
 
 export { Loading } from './Loading';
+export { Breadcrumb } from './Breadcrumb';
 export { CardList } from './CardList';
