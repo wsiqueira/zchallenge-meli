99f7b1c642ce277d23b0d0e30a22e72ad4d663eb
Willian Siqueira <wsiqueira@sogringagem.com.br>
2025-03-23 22:05:49 -0300

    [Utils] Add new images

--- src/utils/AvatarList.ts
+++ src/utils/AvatarList.ts
@@ -1,5 +1,10 @@
 export const avatarList = {
     ['planets']: 'https://img.icons8.com/?size=256&id=a0QK6hpx9ajC',
     ['Luke Skywalker']: 'https://img.icons8.com/?size=256&id=39424',
+    ['C-3PO']: 'https://img.icons8.com/?size=256&id=31823',
+    ['R2-D2']: 'https://img.icons8.com/?size=256&id=39423',
     ['Darth Vader']: 'https://img.icons8.com/?size=256&id=35734',
+    ['Chewbacca']: 'https://img.icons8.com/?size=256&id=89771',
+    ['Yoda']: 'https://img.icons8.com/?size=256&id=68107',
+    ['Boba Fett']: 'https://img.icons8.com/?size=256&id=sIw26ij8G9Ph',
 }
