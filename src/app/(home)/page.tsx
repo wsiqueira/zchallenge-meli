e4bf1010721530f0d6f9787baeb62a5cda72ef69
Willian Siqueira <wsiqueira@sogringagem.com.br>
2025-03-23 23:13:29 -0300

    [Page] Add input search

--- src/app/(home)/page.tsx
+++ src/app/(home)/page.tsx
@@ -1,17 +1,31 @@
 'use client';
 
+import { useState, useEffect } from 'react';
 import { useSearchParams } from 'next/navigation';
 import { useGetData } from '@/hooks';
+
+import { Input } from '@/components/ui/input';
 import { CardList, Pagination, Loading } from '@/components';
 
 export default function PageHome() {
   const searchParams = useSearchParams();
   const page = searchParams.get('page') || '1';
 
-  const { data, isLoading } = useGetData({
+  const { data, isLoading, refetch } = useGetData({
     param: 'people',
     query: `?page=${page}`,
   });
+
+  const [searchTerm, setSearchTerm] = useState('');
+  const dataFiltered = data?.results?.filter((item) => {
+    if (item.name.toLowerCase().includes(searchTerm.toLowerCase())) {
+      return item;
+    }
+  });
+
+  useEffect(() => {
+    refetch();
+  }, [page, refetch]);
 
   return (
     <main className="container grid place-content-center gap-8 mx-auto md:max-w-6xl">
@@ -19,8 +33,18 @@ export default function PageHome() {
         <Loading variant="lightsaber" />
       ) : (
         <>
-          <div className="flex justify-end">[INPUT]</div>
-          <CardList data={data?.results} />
+          {data && (
+            <div className="flex justify-end">
+              <Input
+                type="search"
+                placeholder="Search by name"
+                className="max-w-48"
+                onChange={(event) => setSearchTerm(event.target.value)}
+              />
+            </div>
+          )}
+
+          <CardList data={dataFiltered} />
 
           {data && <Pagination pages={data.count} />}
         </>
