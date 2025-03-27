2c2236a9b9b38e44f095487785ed9a89d7b1ed12
Willian Siqueira <wsiqueira@sogringagem.com.br>
2025-03-27 00:14:13 -0300

    [Comp] LinkList

--- src/components/index.ts
+++ src/components/index.ts
@@ -6,3 +6,5 @@ export { Breadcrumb } from './Breadcrumb';
 export { Pagination } from './Pagination';
 export { CardList } from './CardList';
 export type { CardItemType, CardListType } from './CardList/types';
+export { LinkList } from './LinkList';
+export type { LinkListType } from './LinkList/types';
