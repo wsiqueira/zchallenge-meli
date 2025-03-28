11aafeaececce4d8608804b946bc3a0401d85096
Willian Siqueira <wsiqueira@sogringagem.com.br>
2025-03-27 21:26:01 -0300

    [Config] Add remotePatterns

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,18 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
-    domains: ['img.icons8.com', 'picsum.photos'],
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'img.icons8.com',
+        pathname: '**',
+      },
+      {
+        protocol: 'https',
+        hostname: 'picsum.photos',
+        pathname: '**',
+      },
+    ],
   },
 };
 
